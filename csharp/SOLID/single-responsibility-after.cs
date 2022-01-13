8d77405cda963d461d4880abb82346303d3e896f
Oli Wilkins <okwilkins@googlemail.com>
2022-01-13 22:15:38 +0000

    File src loc update

--- csharp/SOLID/single-responsibility-after.cs
+++ csharp/SOLID/single-responsibility-after.cs
@@ -1,4 +1,4 @@
-// This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/single-responsibility-before.py
+// This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/single-responsibility-after.py
 
 namespace SingleResponsibilityAfter {
     public class Order {
