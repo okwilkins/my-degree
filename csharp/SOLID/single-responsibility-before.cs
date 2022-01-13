0ca29b4f2f24627940f4ee3d3266c5ec2da5889d
Oli Wilkins <okwilkins@googlemail.com>
2022-01-13 21:41:49 +0000

    Order Class Init

--- csharp/SOLID/single-responsibility-before.cs
+++ csharp/SOLID/single-responsibility-before.cs
@@ -1,11 +1,22 @@
 // This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/single-responsibility-before.py
 
 namespace SingleResponsibility {
-    class Order {
-        
+    public class Order {
+        public List<string> Items { get; set; }
+        public List<string> Quantities { get; set; }
+        public List<string> Prices { get; set; }
+        public string Status { get; set; }
+
+        public Order() {
+            List<string> Items = new List<string>();
+            List<string> Quantities = new List<string>();
+            List<string> Prices = new List<string>();
+            string Status = "open";
+        }
+
     }
 
-    class Program {
+    public class Program {
         static void Main(string[] args) {
             Console.WriteLine("Program is running!");
         }
