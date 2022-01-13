444b9f75d400ccb2aad4fb53aa68fbf38b8dd730
Oli Wilkins <okwilkins@googlemail.com>
2022-01-13 21:32:43 +0000

    Csharp first example init

---
+++ csharp/SOLID/single-responsibility-before.cs
@@ -0,0 +1,13 @@
+// This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/single-responsibility-before.py
+
+namespace SingleResponsibility {
+    class Order {
+        
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Program is running!");
+        }
+    }
+}
