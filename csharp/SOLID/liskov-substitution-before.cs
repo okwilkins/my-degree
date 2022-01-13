ab8e71d60f0cd51efe8aa1b5ecaa3f3a5dc417bb
Oli Wilkins <okwilkins@googlemail.com>
2022-01-13 22:42:49 +0000

    Name space fix liskov before

--- csharp/SOLID/liskov-substitution-before.cs
+++ csharp/SOLID/liskov-substitution-before.cs
@@ -1,6 +1,6 @@
 //  This file is derived from: https://github.com/ArjanCodes/betterpython/blob/main/9%20-%20solid/liskov-substitution-before.py
 
-namespace LiskovSubstitution {
+namespace LiskovSubstitutionBefore {
     public class Order {
         public List<string> Items { get; set; }
         public List<int> Quantities { get; set; }
